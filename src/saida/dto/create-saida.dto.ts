24b514b50e2111491884adc76e43b32a18f4ee5c
FlaviaRamosdaSilva <didi.flavia@gmail.com>
2024-12-17 01:15:34 -0300

    iniciado a lógica da saida de produtos (vendas/pedidos)

--- src/saida/dto/create-saida.dto.ts
+++ src/saida/dto/create-saida.dto.ts
@@ -12,21 +12,25 @@ import { PedidoProdutoDto } from './saida-produto.dto';
 export class CreateSaidaDto {
   @IsNotEmpty({ message: 'Insira um código de cliente válido' })
   @IsString()
-  clienteId: String;
+  clienteId: string;
 
   @IsNotEmpty({
     message: 'Insira o tipo de cliente, se lojista ou distribuidor',
   })
   @IsString()
-  tipo_cliente: String;
+  tipo_cliente: string;
 
   @IsOptional()
   @IsNumber()
   desconto: number;
 
   @IsOptional()
-  @IsNumber()
-  motivo_desconto: String;
+  @IsString()
+  motivo_desconto: string;
+
+  @IsOptional()
+  @IsString()
+  status_pedido: string;
 
   @IsArray()
   @ValidateNested({ each: true })
--- src/saida/saida.controller.ts
+++ src/saida/saida.controller.ts
@@ -1,7 +1,6 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { SaidaService } from './saida.service';
+import { Body, Controller, Post } from '@nestjs/common';
 import { CreateSaidaDto } from './dto/create-saida.dto';
-import { UpdateSaidaDto } from './dto/update-saida.dto';
+import { SaidaService } from './saida.service';
 
 @Controller('saida')
 export class SaidaController {
@@ -10,25 +9,5 @@ export class SaidaController {
   @Post()
   create(@Body() createSaidaDto: CreateSaidaDto) {
     return this.saidaService.create(createSaidaDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.saidaService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.saidaService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateSaidaDto: UpdateSaidaDto) {
-    return this.saidaService.update(+id, updateSaidaDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.saidaService.remove(+id);
   }
 }
